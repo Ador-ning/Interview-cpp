3bf09cd98e9f11e132e2c2d0ab624a4c60be0a2d
Ador-ning <ning1992@mail.ustc.edu.cn>
2018-03-22 16:37:29 +0800

    1

---
+++ FindandSort.cpp
@@ -0,0 +1,72 @@
+#include <cstdio>
+#include <cstdlib>
+#include <stdexcept>
+
+/*
+提示： 如果是在 排序的数组（或者部分排序的数组）中查找一个数字或者统计数字出现的次数，使用二分查找 
+*/
+
+void Swap(int *a, int *b){
+	int temp;
+	temp = *a;
+	*a = *b; 
+	*b = temp;
+	// printf("a: %d\tb:%d", *a, *b);
+}
+
+
+int RandomInRange(int index1, int index2){
+	if (index1 >= index2){
+		printf("Invalid range index.\n");
+		return -125; 
+	} 
+	else{
+		return (rand() % (index2-index1+1) + index1);
+	}
+	
+}
+
+int Partition(int data[], int length, int start, int end){
+	if (data == NULL || length <= 0 || start < 0 || end >= length){
+		std::logic_error ex("Invalid parameters.\n");
+		throw new std::exception(ex);
+	}
+	
+	int index = RandomInRange(start, end);
+	Swap(&data[index], &data[end]); 
+	
+	int small = start - 1;
+	 
+	for (index = start; index < end; ++index){
+		if (data[index] < data[end]){
+			++small;
+			if (small != index)
+				Swap(&data[index], &data[small]); 
+		}
+	}
+	
+	++small;
+	Swap(&data[small], &data[end]);
+
+	return small;	
+} 
+
+
+void QuickSort(int data[], int length, int start, int end){
+	if (start == end)
+		return;
+	
+	int index = Partition(data, length, start, end);
+	if (index > start)
+		QuickSort(data,length, start, index-1);
+	else (index < end)
+		QuickSort(data, length, index+1, end);
+}
+
+int main(){
+	int a = 0;
+	int b = 9;
+	// Swap(&a,&b);
+	// printf("%d", RandomInRange(a, b)); 
+	return 0;
+}
---
+++ List.h
@@ -0,0 +1,125 @@
+#include <cstdio>
+
+struct ListNode{
+	int m_nKey;
+	ListNode *m_pNext;
+};
+
+// 创建 List 结点 
+ListNode *CreateListNode(int value){
+	ListNode *pNode = new ListNode();
+	pNode->m_nKey = value;
+	pNode->m_pNext = NULL;
+	
+	return pNode; 
+}
+
+// 连接 List结点 
+void ConnectListNodes(ListNode *pCurrent, ListNode *pNext){
+	if (pCurrent == NULL){
+		printf("Error to connect two nodes.\n");
+		return;
+	}
+	
+	pCurrent->m_pNext = pNext;
+}
+
+// 打印 List 结点 
+void PrintListNode(ListNode *pNode){
+	if (pNode == NULL)
+		printf("The node is NULL.\n");
+	else
+		printf("The key in node is %d.\n", pNode->m_nKey);
+}
+
+// 顺序打印 List 
+void PrintList(ListNode *pHead){
+	printf("Print List start.\n");
+	
+	ListNode *pNode = pHead;
+	
+	while (pNode != NULL){
+		printf("%d\t", pNode->m_nKey);
+		pNode = pNode->m_pNext;
+	}
+	
+	printf("\n Print List end.\n");
+}
+
+// 销毁 List 
+void DestroyList(ListNode *pHead){
+	ListNode *pNode = pHead;
+	
+	while (pNode != NULL){
+		pHead = pHead->m_pNext;
+		delete pNode;
+		pNode = pHead; 
+	} 
+}
+
+// 链表尾部添加 List 结点 
+void AddToTail(ListNode **pHead, int value){
+	ListNode *pNew = new ListNode();
+	pNew->m_nKey = value;
+	pNew->m_pNext = NULL;
+	
+	if (*pHead != NULL){
+		ListNode *pNode = *pHead;
+		
+		while ( pNode->m_pNext != NULL)
+			pNode = pNode->m_pNext;
+		
+		pNode->m_pNext = pNew;	
+	}
+	else
+		*pHead = pNew;
+}
+
+// 删除 List 结点
+void RemoveNode(ListNode **pHead, int value){
+	if (pHead == NULL || *pHead == NULL)
+		return;
+	
+	ListNode *pDeleted = NULL;
+	
+	if ( (*pHead)->m_nKey == value ){ // 删除链表头部 
+		pDeleted = *pHead;
+		(*pHead) = (*pHead)->m_pNext; 
+	}
+	else
+	{
+		ListNode *pNode = *pHead;
+		
+		// 移动 
+		while (pNode->m_pNext != NULL && pNode->m_pNext->m_nKey != value)
+			pNode = pNode->m_pNext;
+		
+		// 删除 
+		if ( pNode->m_pNext != NULL && pNode->m_pNext->m_nKey == value){
+			pDeleted = pNode->m_pNext;
+			pNode->m_pNext = pDeleted->m_pNext;
+		}
+	}
+	
+	if (pDeleted != NULL){
+		delete pDeleted;
+		pDeleted = NULL;
+	}
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
