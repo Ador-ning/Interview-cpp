8fc03a32ea2d84a5e68049ef6b1cb6637eb693bf
UpForward <ning1992@mail.ustc.edu.cn>
2018-06-29 16:32:49 +0800

    83

--- leetcode/leet80.h
+++ leetcode/leet80.h
@@ -5,4 +5,24 @@
 #ifndef INTERVIEW_CPP_LEET80_H
 #define INTERVIEW_CPP_LEET80_H
 
+
+// ============================== 题目83 删除链表中重复元素
+ListNode *deleteDuplicates(ListNode *head) {
+
+	if (head == nullptr)
+		return head;
+	ListNode *p = head;
+	ListNode *pDelete = head->m_pNext;
+
+	while (pDelete->m_pNext != nullptr) {
+		if (p->m_nKey == pDelete->m_nKey) {
+			p->m_pNext = pDelete->m_pNext;
+		} else {
+			p = p->m_pNext;
+		}
+		pDelete = pDelete->m_pNext;
+	}
+	return head;
+}
+
 #endif //INTERVIEW_CPP_LEET80_H
