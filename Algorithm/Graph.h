dadf0175e7e5bf26edb4b3c9e009589e520575e3
UpForward <ning1992@mail.ustc.edu.cn>
2018-07-10 22:52:52 +0800

    graph

--- Algorithm/Graph.h
+++ Algorithm/Graph.h
@@ -6,7 +6,71 @@
 #define INTERVIEW_CPP_GRAP_H
 
 #include <iostream>
+#include <queue>
 
 using namespace std;
+
+#define N 15
+
+class Node {
+	int v;  // 连接点编号
+	int w;  // 权值
+
+	Node() : v(0), w(0) {}
+
+	Node(int i, int j) : v(i), w(j) {}
+};
+
+vector<Node> Adj[N]; // 图邻接表存储
+
+bool visit[N] = {false}; // 访问标志
+bool inq[N] = {false}; // 入队列标志
+
+// ======================= 图遍历 ======================= //
+void DFS(int u, int depth) {
+	visit[u] = true;
+	// 对 u 操作
+	for (int i = 0; i < Adj[u].size(); i++) {
+		int v = Adj[u][i];
+		if (visit[v] == false)
+			DFS(v, depth + 1);
+	}
+}
+
+void DfsTrave() {
+	for (int u = 0; u < N; u++) {
+		if (visit[u] == false)
+			DFS(u, 1);
+	}
+}
+
+void BFS(int u) {
+	queue<int> q;
+	q.push(u);
+	inq[u] = true;
+
+	while (!q.empty()) {
+		int u = q.front();
+		q.pop();
+		for (int i = 0; i < Adj[u].size(); i++) {
+
+			int v = Adj[u][i];  // 邻接结点编号
+
+			if (inq[v] == false) {
+				q.push(v);
+				inq[v] = true;
+			}
+		}
+	}
+}
+
+void BfsTrave() {
+	for (int u = 0; u < N; u++) {
+		if (inq[u] == false)
+			BFS(u); // 遍历u所在的连通块
+	}
+}
+
+// ======================= 最短路径 ======================= //
 
 #endif //INTERVIEW_CPP_GRAP_H
